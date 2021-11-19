3dcb07b17b28eb9cc255fc607a3554e6d54a0d53
SemenuykSergo <72045074+SemenuykSergo@users.noreply.github.com>
2021-11-19 21:13:58 +0200

    minor edits

--- script/Console.gd
+++ script/Console.gd
@@ -1,23 +1,5 @@
 # песочница для тестирования скриптов
 
-
-
 extends Node
 
-var arr=[]
-var temp
-var rnd
 
-func _ready():
-	for i in 16:
-		arr.append(i+1)
-		
-	
-	var count=16
-	while (count!=0):
-		rnd=int(rand_range(0,count))
-		temp=arr[rnd]
-		arr[rnd]=arr[count-1]
-		arr[count-1]=temp
-		count-=1
- 
--- script/Field.gd
+++ script/Field.gd
@@ -1,5 +1,27 @@
 extends Node
 
-func _on_Button_button_down():	
-	print(get_index())# получаем индекс массива выведенных полей	
-	print(get_node("fieldItem/tab").text)#содержимое клетки
+var localArr=[]
+#var tempValueArr
+#var position
+#var itemField
+
+
+func _on_button_button_down():
+	
+	localArr=GlobalArr.globalArr	
+	
+	#print(get_index())# получаем индекс массива выведенных полей	
+	#print(get_node("fieldItem/tab").text)#содержимое клетки
+	#print(get_node("fieldItem/tab").get_instance_id())
+	print(GlobalArr.fieldId)
+	
+	#position=get_index()-1
+	#item=get_node("fieldItem/tab")
+	
+	
+	#tempValueArr=item
+	#localArr[position]=
+	#localArr[localArr.size-1]=tempValueArr
+	
+	
+	instance_from_id(1294).text="NO"
